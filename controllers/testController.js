d8bfea710114361fd926d916146c1b02d941f1b9
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-07-14 09:59:10 -0400

    deployed to heroku

--- controllers/testController.js
+++ controllers/testController.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const test = require("../models/test");
 
 router.get("/", (req, res) => {
-  test.all();
+  test.all(function (data) {
+    console.log(data);
+  });
   res.send("Hello world!");
 });
 
--- server.js
+++ server.js
@@ -1,28 +1,46 @@
+/**
+ * REQUIRE NPM PACKAGES
+ * REQUIRE EXTERNAL FILES
+ */
+
 const express = require("express");
+const exphbs = require("express-handlebars");
+const morgan = require("morgan");
 const TestController = require("./controllers/testController");
 
+/**
+ * DEFINE VARIABLES
+ */
 const PORT = process.env.PORT || 8080;
-
 const app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
+/**
+ * MIDDLEWARE
+ */
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Set Handlebars.
-const exphbs = require("express-handlebars");
-
+// Handlebars setup
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+app.use(morgan())
+/**
+ * VIEW ROUTES
+ * API ROUTES
+ */
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Routes
 app.use(TestController);
 
+/**
+ * APP LISTEN
+ */
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
-    console.log(process.env.GOOGLE_API_KEY);
+  console.log(process.env.GOOGLE_API_KEY);
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
